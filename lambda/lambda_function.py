070c32aeb59d3c685d505609c8988c80a2ca05d6
M4V3R1CK <jonathangrismond@gmail.com>
2023-10-17 13:55:59 -0300

    "update" to gpt-3.5

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,13 +1,13 @@
 import logging
-import ask_sdk_core.utils as ask_utils
+
 import openai
-from ask_sdk_core.skill_builder import SkillBuilder
-from ask_sdk_core.dispatch_components import AbstractRequestHandler
-from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.dispatch_components import (AbstractExceptionHandler,
+                                              AbstractRequestHandler)
 from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_model import Response
 
-# Set your OpenAI API key
+# Configure your OpenAI API key
 openai.api_key = "substitua-pela-sua-chave-da-openai"
 
 logger = logging.getLogger(__name__)
@@ -18,14 +18,10 @@ messages = [{"role": "system", "content": "Você é uma assistente muito útil. 
 class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
     def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-
         return ask_utils.is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
         speak_output = "Bem vindo ao Chat 'Gepetê Quatro' da 'Open ei ai'! Qual a sua pergunta?"
-
         return (
             handler_input.response_builder
                 .speak(speak_output)
@@ -36,29 +32,25 @@ class LaunchRequestHandler(AbstractRequestHandler):
 class GptQueryIntentHandler(AbstractRequestHandler):
     """Handler for Gpt Query Intent."""
     def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
         return ask_utils.is_intent_name("GptQueryIntent")(handler_input)
 
     def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
         query = handler_input.request_envelope.request.intent.slots["query"].value
         response = generate_gpt_response(query)
 
         return (
-                handler_input.response_builder
-                    .speak(response)
-                    .ask("Alguma outra pergunta?")
-                    .response
-            )
+            handler_input.response_builder
+                .speak(response)
+                .ask("Alguma outra pergunta?")
+                .response
+        )
 
 class CatchAllExceptionHandler(AbstractExceptionHandler):
     """Generic error handling to capture any syntax or routing errors."""
     def can_handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> bool
         return True
 
     def handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> Response
         logger.error(exception, exc_info=True)
 
         speak_output = "Desculpe, não consegui obter uma resposta para esta pergunta. Tente perguntar de outra forma."
@@ -73,12 +65,10 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
 class CancelOrStopIntentHandler(AbstractRequestHandler):
     """Single handler for Cancel and Stop Intent."""
     def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
         return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
                 ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
 
     def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
         speak_output = "Saindo do modo Chat Gepetê."
 
         return (
@@ -93,7 +83,7 @@ def generate_gpt_response(query):
             {"role": "user", "content": query},
         )
         response = openai.ChatCompletion.create(
-            model="gpt-4",
+            model="gpt-3.5-turbo",  # Use o modelo GPT-3.5 Turbo
             messages=messages,
             max_tokens=1000,
             n=1,
@@ -112,5 +102,3 @@ sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(GptQueryIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_exception_handler(CatchAllExceptionHandler())
-
-lambda_handler = sb.lambda_handler()
